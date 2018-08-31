89529bcdfefe9c72d055d9fd14f3d50b453ece64
João Paulo Cossi <jppcossi@gmail.com>
2018-08-31 12:29:06 -0300

    dia 31/08 - final

--- src/decaf/DecafLexer.g4
+++ src/decaf/DecafLexer.g4
@@ -16,21 +16,49 @@ tokens
 
 LCURLY : '{';
 RCURLY : '}';
+TRAÇO: '-';
+VIRG: ',';
+MAS: '+';
+MULT: '*';
+PONTVIR: ';';
+LCOLCHE: '[';
+RCOLCHE: ']';
+OU: '||';
+DOISIGUAL: '==';
+IGUAL: '=';
+DIF: '!=';
+LPARENT: '(';
+RPARENT: ')';
+ECOMP: '&&';
+COMPMENOR: '<';
+COMPMAIOR: '>';
+COMPMENORIGUAL: '<=';
+COMPMAIORIGUAL: '>=';
 
-ID  :
-  ('a'..'z' | 'A'..'Z')+;
-
-WS_ : (' ' | '\n' ) -> skip;
-
-SL_COMMENT : '//' (~'\n')* '\n' -> skip;
+CALLOUT: 'callout';
+CLASS: 'class';
+ELSE: 'else';
+BOOLEAN: 'boolean';
+IF: 'if';
+INT: 'int';
+RETURN: 'return';
+VOID: 'void';
+FOR: 'for';
+BREAK: 'break';
+CONTINUE: 'continue';
 
 CHAR : '\'' (ESC|ASC) '\'';
 STRING : '"' (ESC|ASC)* '"';
 NUMBER : '-'? [0-9]+;
-OPERATORS: ( '+' | '-' |'*' |'<' |'<=' | '!=' | '&&');
+BOOLEANLITERAL: ('false' | 'true');
+HEXA: '0x'('A'..'f' | 'a'..'f' | '0'..'9')+;
 
-fragment
-ESC :  '\\' ('n'|'"'| 't'| '\\'| '\'');
+fragment ESC :  '\\' ('n'|'"'| 't'| '\\'| '\'');
+fragment ASC: [\u0020-\u0021|\u0023-\u0026|\u0028-\u005B|\u005D-\u007E];
+fragment NUMB: ('0'..'9');
+fragment MAIS: ('A'..'Z');
+fragment MINUS: ('a'..'z');
 
-fragment
-ASC: [\u0020-\u0021|\u0023-\u0026|\u0028-\u005B|\u005D-\u007E];
+ID  : ( MAIS | MINUS | '_')(MAIS | MINUS | '_' | NUMB)*;
+WS_ : (' ' | '\n'| '\t') -> skip;
+SL_COMMENT : '//' (~'\n')* '\n' -> skip;
--- src/decaf/Main.java
+++ src/decaf/Main.java
@@ -10,65 +10,67 @@ import java6035.tools.CLI.*;
 class Main {
     public static void main(String[] args) {
         try {
-        	CLI.parse (args, new String[0]);
+            CLI.parse (args, new String[0]);
 
-        	InputStream inputStream = args.length == 0 ?
+            InputStream inputStream = args.length == 0 ?
                     System.in : new java.io.FileInputStream(CLI.infile);
 
-        	if (CLI.target == CLI.SCAN)
-        	{
-        		DecafLexer lexer = new DecafLexer(new ANTLRInputStream(inputStream));
-        		Token token;
-        		boolean done = false;
-        		while (!done)
-        		{
-        			try
-        			{
-		        		for (token=lexer.nextToken(); token.getType()!=Token.EOF; token=lexer.nextToken())
-		        		{
-		        			String type = "";
-		        			String text = token.getText();
+            if (CLI.target == CLI.SCAN)
+            {
+                DecafLexer lexer = new DecafLexer(new ANTLRInputStream(inputStream));
+                Token token;
+                boolean done = false;
+                while (!done)
+                {
+                    try
+                    {
+                        for (token=lexer.nextToken(); token.getType()!=Token.EOF; token=lexer.nextToken())
+                        {
+                            String type = "";
+                            String text = token.getText();
 
-		        			switch (token.getType())
-		        			{
-		        			case DecafLexer.ID:
-		        				type = " IDENTIFIER";
-		        				break;
-						case DecafLexer.CHAR:
-							type = " CHARLITERAL";
-							break;
-						case DecafLexer.STRING:
-							type = " STRINGLITERAL";
-							break;
-						case DecafLexer.NUMBER:
-							type = " INTLITERAL";								
-							break;
-						case DecafLexer.OPERATORS:
-							type = " OPERATOR";
-							break;
-		        			}
-		        			System.out.println (token.getLine() + type + " " + text);
-		        		}
-		        		done = true;
-        			} catch(Exception e) {
-        	        	// print the error:
-        	            System.out.println(CLI.infile+" "+e);
-        	            lexer.skip();
-        	        }
-        		}
-        	}
-        	else if (CLI.target == CLI.PARSE || CLI.target == CLI.DEFAULT)
-        	{
-        		DecafLexer lexer = new DecafLexer(new ANTLRInputStream(inputStream));
-				CommonTokenStream tokens = new CommonTokenStream(lexer);
-        		DecafParser parser = new DecafParser(tokens);
+                            switch (token.getType())
+                            {
+                            case DecafLexer.ID:
+                                type = " IDENTIFIER";
+                                break;
+                            case DecafLexer.CHAR:
+                                type = " CHARLITERAL";
+                                break;
+                            case DecafLexer.STRING:
+                                type = " STRINGLITERAL";
+                                break;
+                            case DecafLexer.NUMBER:
+                                type = " INTLITERAL";
+                                break;
+                            case DecafLexer.BOOLEANLITERAL:
+                                type = " BOOLEANLITERAL";
+                                break;
+                            case DecafLexer.HEXA:
+                                    type = " INTLITERAL";
+                                    break;
+                            }
+                            System.out.println (token.getLine() + type + " " + text);
+                        }
+                        done = true;
+                    } catch(Exception e) {
+                        // print the error:
+                        System.out.println(CLI.infile+" "+e);
+                        lexer.skip();
+                    }
+                }
+            }
+            else if (CLI.target == CLI.PARSE || CLI.target == CLI.DEFAULT)
+            {
+                DecafLexer lexer = new DecafLexer(new ANTLRInputStream(inputStream));
+                CommonTokenStream tokens = new CommonTokenStream(lexer);
+                DecafParser parser = new DecafParser(tokens);
                 parser.program();
-        	}
-        	
+            }
+
         } catch(Exception e) {
-        	// print the error:
+            // print the error:
             System.out.println(CLI.infile+" "+e);
         }
     }
 }
-
